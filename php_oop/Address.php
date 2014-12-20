7251d2af09c62bbb5df3ac951b04f32ce217d848
leihro <lei036072728@gmail.com>
2014-12-20 17:17:25 +0100

    magic method : __get, __set, __construct, __toString

--- php_oop/Address.php
+++ php_oop/Address.php
@@ -8,7 +8,7 @@
     public $extra_address;
 
     //post code
-    public $postcode;
+    protected $_postcode;
 
     //city name
     public $city_name;
@@ -19,17 +19,78 @@
     //country name
     public $country_name;
 
+    protected $_address_id;
+
+    protected $_time_created;
+    protected $_time_updated;
+
+    //constructor
+    function __construct($data = array()){
+      $this->_time_created = time();
+
+      //Ensure that the Address can be populated
+      if(!is_array($data)){
+        trigger_error("Unable to construct address with a " . get_class($name));
+      }
+
+      // init the obj
+      if(count($data) > 0){
+        foreach ($data as $name => $value) {
+          if(in_array($name, array('time_created', 'time_updated'))){
+            $name = '_' . $name;
+          }
+          $this->$name = $value;
+        }
+      }
+    } 
+
+    //magic __get
+    function __get($name){
+      if(!$this->_postcode){
+        $this->_postcode = $this->guess_postcode();
+      }
+      $protected_name = '_' . $name;
+      if(property_exists($this, $protected_name)){
+        return $this->$protected_name;
+      }
+
+      trigger_error("Undefined property via __get: " . $name);
+      return NULL;
+    }
+
+    //magic __set
+    function __set($name, $value){
+      if($name == "postcode"){
+        $this->$name = $value;
+        return;
+      }
+
+      trigger_error("Undefined or unallowed property via __set: " . $name);
+      return NULL;
+    }
+
+    //magic __tostring
+    function __toString(){
+      return $this->display();
+    }
+
+    protected function guess_postcode(){
+      return "Guess value from DB";
+    }
+
     public function display(){
       $output = "";
       $output .= $this->physical_address;
-      if (isset($this->extra_address)) {
+      if ($this->extra_address) {
         $output .= ", " . $this->extra_address;
       }
       $output .= "<br>";
-      $output .= $this->postcode;
-      $output .= ", " . $this->city_name;
-      $output .= ", " . $this->province;
-      $output .= ", " . $this->country_name;
+      $output .= $this->postcode . ", ";
+      $output .= $this->city_name . ", ";
+      $output .= $this->province . ", ";
+      $output .= $this->country_name;
       return $output;
     }
+
+
   }
--- php_oop/index.php
+++ php_oop/index.php
@@ -18,6 +18,38 @@
  	<meta charset ="utf-8">
  </head>
  <body>
- 	<?php  echo $address->display(); ?>
+
+ 	<h3>Testing Constructor</h3>
+ 	<?php 
+ 		$address2 = new Address(array(
+ 			'physical_address' => 'Jiansheli 8',
+ 			'postcode' => '066001',
+ 			'city_name' => 'Qinhuangdao',
+ 			'province' => 'Hebei',
+ 			'country_name' => 'China',
+ 			));
+ 		echo $address2;
+ 	 ?>
+
+ 	<h3>Testing magic __set and __get</h3>
+ 	<?php  
+ 		unset($address->postcode);
+ 		echo $address->display(); 
+ 	?>
+ 	<br>
+
+ 	<h3>display address</h3>
+ 	<?php  
+ 		$address->postcode = 18109;
+ 		echo $address->display(); 
+ 	?>
+ 	<br>
+
+ 	<h3>dump all vars in address obj</h3>
+ 	<?php 
+ 		echo "<tt><pre>";
+ 		echo var_export($address, TRUE);
+ 		echo "</pre></tt>";
+ 	 ?>
  </body>
  </html>
