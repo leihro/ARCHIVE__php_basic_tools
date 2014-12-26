c85340e6c200ed67fba56a99aa04e69ef78645c6
leihro <lei036072728@gmail.com>
2014-12-26 20:24:25 +0100

    update file upload, setting max size, allowed types and extensions, move file from tmp to certain dir, sanitize filename given from users

--- security/file/upload/upload_demo.php
+++ security/file/upload/upload_demo.php
@@ -9,6 +9,7 @@
  <body>
  	<form action = "" method = "POST" enctype = "multipart/form-data">
  		<p>Choose a File to upload</p>
+ 		<input type = "hidden" name = "MAX_FILE_SIZE" value = "<?php echo $file->max_file_size; ?>">
  		<input type = "file" name = "my_file" /><br />
  		<input type = "submit" name = "submit" value = "Upload File" />
  	</form>
