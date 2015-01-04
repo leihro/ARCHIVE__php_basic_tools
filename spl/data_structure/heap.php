18910939cbf5a8d0fc5299e990c8015e96c8b52a
leihro <lei036072728@gmail.com>
2015-01-04 13:40:55 +0100

    update heap.php add splheap

--- spl/data_structure/heap.php
+++ spl/data_structure/heap.php
@@ -7,6 +7,55 @@
  */
 
 /**
+ * Class SortCourses
+ */
+class SortCourses extends SplHeap {
+    /**
+     * return a MinHeap
+     * @param mixed $val1
+     * @param mixed $val2
+     * @return ints ASC order
+     */
+    protected function compare($val1, $val2){
+        $val1 = (string) $val1->title;
+        $val2 = (string) $val2->title;
+        if($val1 == $val2){
+            return 0;
+        } elseif($val1 < $val2){
+            return 1;
+        } else {
+            return -1;
+
+        }
+    }
+}
+
+
+//test of xml data
+//$data = simplexml_load_file('../../common/data/courses.xml');
+
+
+//test of json data
+$data = file_get_contents('../../common/data/courses.json');
+$data = json_decode($data);
+
+/**
+ * init a self defined heap, which will sort values automatically
+ */
+$courses = new SortCourses();
+foreach($data as $item){
+    // it will sort automatically based on the title of each item
+    $courses->insert($item);
+}
+foreach($courses as $course) {
+    echo "<h4>$course->title with $course->author</h4>";
+    echo "<p>$course->description</p>";
+}
+unset($data);
+unset($courses);
+echo "<hr />";
+
+/**
  * MinHeap: binary tree, first value is the smallest
  * MaxHeap: binary tree, first value is the largest
  */
